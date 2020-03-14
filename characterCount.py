e7b3fa5736761cf2305786e6526180d0af4c3677
xysey <xysey6@gmail.com>
2020-03-14 20:48:26 +0800

    edited character count

--- characterCount.py
+++ characterCount.py
@@ -1,12 +1,18 @@
-message = ""
+message = '''
+If you import the pprint module into your programs, you’ll have access to
+the pprint() and pformat() functions that will “pretty print” a dictionary’s
+values. This is helpful when you want a cleaner display of the items in a
+dictionary than what print() provides. Modify the previous characterCount.py
+program and save it as prettyCharacterCount.py.
+'''
 
 countDict = {}
 
 #count all the characters and add them to the count dictionary
 #character as the key and the count as the value
 for character in message:
-    count.setdefault(character, 0)
-    count[character] = count[character] + 1
+    countDict.setdefault(character, 0)
+    countDict[character] = countDict[character] + 1
 
 #print all the characters and their count in a nice way
 for character, count in countDict.items():
