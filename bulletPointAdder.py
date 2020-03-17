5ac035fff159479b707b95ae5f4eb64c2f8df009
xysey <xysey6@gmail.com>
2020-03-17 18:21:12 +0800

    bullet point adder adde

---
+++ bulletPointAdder.py
@@ -0,0 +1,16 @@
+#! python3
+
+#Paste text from the clipboard
+import pyperclip
+
+text = pyperclip.paste()
+
+#Do something about it
+text = text.split('\n')
+for i in range(len(text)):
+    text[i] = '* ' + text[i]
+
+#Copy the text to the clipboard
+pyperclip.copy('\n'.join(text))
+
+
