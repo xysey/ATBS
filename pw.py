a5e68d4d87c4e47ddb49984bb09450ad48b078e5
xysey <xysey6@gmail.com>
2020-03-17 17:02:25 +0800

    added a gitignore and the insecure password manager

---
+++ pw.py
@@ -0,0 +1,21 @@
+#! python3
+
+# pw.py an insecure password manager
+
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+             'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+             'luggage': '12345'}
+
+import sys, pyperclip
+
+if len(sys.argv) < 2:
+    print("Usage: python pw.py [account] - copy the account password")
+    sys.exit()
+
+account = sys.argv[1]
+
+if account not in PASSWORDS:
+    print('There is no such account existing in my memory')
+    sys.exit()
+else:
+    pyperclip.copy(PASSWORDS[account])
